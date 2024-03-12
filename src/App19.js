75fe2ea52d4ca8e94b5ff3cd150a171c66d5f082
npx <srikanthkeloth@lyrostech.com>
2024-03-12 16:27:12 +0530

    71times

--- src/App19.js
+++ src/App19.js
@@ -1,14 +1,28 @@
-import { useState, createContext, useContext } from "react";
-import Childa from "./Childa";
-export const UserContext = createContext();
-export default function App13a() {
-  const [user, setUser] = useState("John");
+// import { useState, createContext, useContext } from "react";
+// import Childa from "./Childa";
+// export const UserContext = createContext();
+// export default function App13a() {
+//   const [user, setUser] = useState("John");
+//   return (
+//     <>
+//       <UserContext.Provider value={{ user, setUser }}>
+//         <h2>Hello {user} from App13 component</h2>
+//         <Childa />
+//       </UserContext.Provider>
+//     </>
+//   );
+// }
+
+
+import React from "react";
+import App19child from "./App19child";
+export default function App19() {
   return (
-    <>
-      <UserContext.Provider value={{ user, setUser }}>
-        <h2>Hello {user} from App13 component</h2>
-        <Childa />
-      </UserContext.Provider>
-    </>
+    <div>
+      <App19child>
+        <h1>Hello World</h1>
+        <p>This is a paragraph</p>
+      </App19child>
+    </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App5 from "./App5";
-import App19 from "./App19";
-import App13a from "./App13a";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <App19 />
-    <App13a />
+    <App20 />
+   
   </>
 );
